e598e652e9c693d70323a7351d3aeadf534a044e
master <max@mail.ru>
2016-03-14 22:30:04 +0200

    javascript

---
+++ app/scripts/controllers/strict.js
@@ -0,0 +1,15 @@
+
+(function(){
+var num=123;
+console.log(num);
+console.log('I can..');
+
+var ob1={
+    name:"John",
+    name:"Bruce"
+};
+
+console.log(ob1);// 'use strict';
+
+// Strict mode 
+})();
